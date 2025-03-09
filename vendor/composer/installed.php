6da577c9fcb4ac1514e653f7aaf4e4679569527b
Mahbub Hasan Imon <mahbubhasanimon@gmail.com>
2025-03-09 15:32:16 +0600

    Added Vendor Folder

---
+++ vendor/composer/installed.php
@@ -0,0 +1,43 @@
+<?php return array(
+    'root' => array(
+        'name' => 'mhimon/ultra-elementor-addons',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '0a6b14764fe0bd617630adac6b642d0ccb3edb82',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'freemius/wordpress-sdk' => array(
+            'pretty_version' => '2.11.0',
+            'version' => '2.11.0.0',
+            'reference' => '9ad5477e4f0a39999b96bb21d0327bc6841d4590',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../freemius/wordpress-sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'mhimon/ultra-elementor-addons' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '0a6b14764fe0bd617630adac6b642d0ccb3edb82',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'pablo-sg-pacheco/wp-namespace-autoloader' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '069163f215743c83381613749ace0c5a642720b4',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../pablo-sg-pacheco/wp-namespace-autoloader',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+    ),
+);
